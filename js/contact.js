0db7b047c9bdf59fc4eef9c145dffaeb8a66ba5c
chereshnya3000 <chereshnya3000@yandex.ru>
2015-11-25 01:31:18 +0200

    New!!!

---
+++ js/contact.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function() { // Ждём загрузки страницы
+	
+	$(".contact_bg").click(function(){	// Событие клика на затемненный фон	   
+		$(".contact").fadeOut(800);	// Медленно убираем всплывающее окно
+	});
+
+});
+
+function showContact() {
+	$(".contact").fadeIn(800); // Медленно выводим изображение
+}
